31ade743f48cb61cbae980917f88719188d5c27c
David Tellander <dtellander@gmail.com>
2013-01-06 21:28:43 +0100

    Restructuring the repository a little

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-. e:\utils\posh-hg\profile.example.ps1
-
-# Aliases
-Set-Alias ss Select-String
-Set-Alias subl "e:\Program Files\Sublime Text 2\sublime_text.exe"
-
-# Modules
-Import-Module Derantell 
